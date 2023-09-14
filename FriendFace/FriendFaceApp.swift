927cc4da5c4efd847c62f50af53de6da4502051b
Ojidaemo <vit.martsinovich@gmail.com>
2023-09-14 15:51:25 +0300

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Vitali Martsinovich on 2023-09-14.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
